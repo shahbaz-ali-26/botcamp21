c0a666b7770fd6829c0b4a1dcf6fcf6afa16cc0a
shahbaz-ali-26 <shahbazgb2@gmail.com>
2021-01-28 16:18:26 +0500

    updated

---
+++ src/dinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Dinner(props){
+    return(
+        <div>
+            <h1>hello everyone{props.dishname}</h1>
+    <h1>hello everyone{props.sweetdish}</h1>
+    <h1>hello everyone{props.dishname}</h1>
+        </div>
+    )
+}
+export default Dinner;
