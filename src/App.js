0c5aa0ad0d1af577d4867b6561ec9e93ee3ea053
shahbaz-ali-26 <shahbazgb2@gmail.com>
2021-01-29 19:27:12 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
+import React ,{useState} from 'react'
 import Dinner from './dinner.js';
+import  './App.css';
 function App() {
+  let [count , setcount]= useState(1);
+  let [isMorning , setMorning]= useState(true);
   return (
-<Dinner dishname="baryani"sweetdish="sweet"/>
+    <div className={`box ${isMorning ? 'daylight' :''}`}>
+      <h1>Good{isMorning ? 'Morning' :'Night'}</h1>
+<Dinner conter={count}/>
+<br/>
+<button onClick={()=> setcount(count+1)}>Updated</button>
+<button onClick={()=> setMorning(!isMorning)}>chang_day</button>
+</div>
   );
 }
 
--- src/dinner.js
+++ src/dinner.js
@@ -3,9 +3,7 @@ import React from 'react'
 function Dinner(props){
     return(
         <div>
-            <h1>hello everyone{props.dishname}</h1>
-    <h1>hello everyone{props.sweetdish}</h1>
-    <h1>hello everyone{props.dishname}</h1>
+          <h1>Value of counter variable is:{props.conter}</h1>
     
         </div>
     )
